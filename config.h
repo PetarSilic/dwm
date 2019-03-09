a22facb4a756511dd89df536df0aba1a3022b152
Petar Silic <petar.silic@gmail.com>
2019-03-09 18:21:45 +0100

    dev script is in ~/test/dwmtraybar

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -21,12 +21,12 @@ static const char *colors[][3]      = {
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
-	[SchemeNorm] = { 0xffU, 0xc0, 0xffU },
-	[SchemeSel]  = { 0xffU, 0xc0, 0xffU },
+	[SchemeNorm] = { 0xffU, 0xc0, 0x00U },
+	[SchemeSel]  = { 0xffU, 0xc0, 0x00U },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -63,11 +63,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
 static const char *sndup[]  = { "/home/Petar/s/up", NULL };
 static const char *snddown[]  = { "/home/Petar/s/down", NULL };
-static const char *nautilus[]  = { "/usr/bin/nautilus", NULL };
 static const char *nautilus[]  = { "/usr/bin/nautilus", NULL };
 static const char *appfinder[]  = { "/usr/bin/xfce4-appfinder", NULL };
 
@@ -130,3 +129,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
